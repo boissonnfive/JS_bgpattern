dbb37279539cf34d8e3c0a1189b05f7b04939553
boissonnfive <boissonnfive@gmail.com>
2009-10-25 17:22:19 +0000

    Ajout de commentaires.

--- javascript/bgpattern.js
+++ javascript/bgpattern.js
@@ -1,4 +1,9 @@
-
+// Ce script permet de récupérer la valeur d'une option dans une balise
+// "select" et utilise cette valeur pour changer l'image de fond de la page.
+// Pour utiliser ce script, il faut:
+// un formulaire avec l'id "bgpattern"
+// une liste déroulante (select) avec l'id "list"
+// les valeurs des options qui indiquent le chemin de l'image
 
 function switchBGPattern() {
 	var n=document.bgpattern.list.selectedIndex;
